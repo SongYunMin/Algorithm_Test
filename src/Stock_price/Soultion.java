50c2f6022e22ca5e55473cc6994df3872539ef47
YunMin-Song <thd930308@naver.com>
2020-09-05 23:30:24 +0900

    Stock Price Example Update

---
+++ src/Stock_price/Soultion.java
@@ -0,0 +1,35 @@
+package Stock_price;
+
+class Solution {
+    // prices : 초 단위로 기록된 주식가격
+    // answer : 가격이 떨어지지 않은 기간을 담을 배열
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        int count = 0;
+
+        for(int i=0;i<prices.length;i++){
+            for(int j=i;j<prices.length-1;j++){
+                if(prices[i] <= prices[j+1]){
+                    count++;
+                    if(j == prices.length-2){
+                        answer[i] = count;
+                        count=0;
+                        break;
+                    }
+                }else{
+                    count++;
+                    answer[i] = count;
+                    count=0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] test = {1,2,3,2,3};
+        s.solution(test);
+    }
+}
--- src/an_uncompleted_player/Solution.java
+++ src/an_uncompleted_player/Solution.java
@@ -18,3 +18,23 @@ class Solution {
         return participant[i];
     }
 }
+
+
+// 다른 사람의 풀이
+
+//class Solution {
+//    public boolean solution(String[] phone_book) {
+//        boolean answer = true;
+//
+//        for(int i =0; i<phone_book.length; i++){
+//            for(int j =0; j<phone_book.length; j++){
+//                if(i == j) continue;
+//                if(phone_book[j].indexOf(phone_book[i])==0){//접두어라면
+//                    return false;
+//                }
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
